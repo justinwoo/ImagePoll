2088dcde23fced5c2e0bf5dc1426c88d2b63a3e9
Justin Woo <moomoowoo@gmail.com>
2014-03-06 19:02:21 -0600

    tore out some yeoman boilerplate

--- app/client/test/spec/controllers/main.js
+++ app/client/test/spec/controllers/main.js
@@ -16,7 +16,7 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('One should equal one.', function () {
+    expect(1).toBe(1);
   });
 });
